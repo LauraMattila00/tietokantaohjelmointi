c7e53aa627749cb192d45337ae3519a15f846f41
LauraMattila00 <n1mala00@students.oamk.fi>
2022-12-19 13:32:01 +0200

    tuotteiden haku

---
+++ products/search.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+$parameters = explode('/',$uri);
+$phrase = $parameters[1];
+
+try {
+    $db = openDb();
+    $sql = "select * from tuote where tuotenimi like '%$phrase%'";
+    selectAsJson($db, $sql);
+} catch (PDOException $e) {
+    returnError($e);
+}
