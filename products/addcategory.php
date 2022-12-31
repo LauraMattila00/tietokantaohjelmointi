f0819ea9c451d5bd5b5ad52551133a2b05ef2412
saijaris <saija.ristolainen@gmail.com>
2022-12-31 14:26:01 +0200

    Check session in addcategory

--- products/addcategory.php
+++ products/addcategory.php
@@ -3,17 +3,23 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
-$input = json_decode(file_get_contents('php://input'));
-$name = filter_var($input->name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+if(!isset($_SESSION["username"])){
+    http_response_code(403);
+    echo "Ei käyttöoikeutta!";
+} else {
 
-try {
-    if(isset($name)){
-    $db = openDb();
-    $sql = "insert into tuoteryhma (trnimi) values ('$name')";
-    executeInsert($db, $sql);
-    $data = array('trnro' => $db->lastInsertId(), 'trnimi' => $name);
-    print json_encode($data);
-    } 
-} catch (PDOException $pdoex) {
-returnError($pdoex);
+    $input = json_decode(file_get_contents('php://input'));
+    $name = filter_var($input->name, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    try {
+        if(isset($name)){
+            $db = openDb();
+            $sql = "insert into tuoteryhma (trnimi) values ('$name')";
+            executeInsert($db, $sql);
+            $data = array('trnro' => $db->lastInsertId(), 'trnimi' => $name);
+            print json_encode($data);
+        } 
+    } catch (PDOException $pdoex) {
+    returnError($pdoex);
+    }
 }
